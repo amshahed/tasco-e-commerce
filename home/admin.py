ad64a777e15047173c68d76d0b8f436e7cd8773c
ams-hasan <amsh.himu@gmail.com>
2021-02-05 20:37:37 +0600

    initial commit

---
+++ home/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+# Register your models here.
+from home.models import Setting, ContactMessage, FAQ, SettingLang
+
+
+class SettingtAdmin(admin.ModelAdmin):
+    list_display = ['title','company', 'update_at','status']
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ['name','subject', 'update_at','status']
+    readonly_fields =('name','subject','email','message','ip')
+    list_filter = ['status']
+
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ['question', 'answer','ordernumber','status']
+    list_filter = ['status']
+
+
+class SettingLangAdmin(admin.ModelAdmin):
+    list_display = ['title', 'keywords','description']
+
+admin.site.register(Setting,SettingtAdmin)
+admin.site.register(ContactMessage,ContactMessageAdmin)
+admin.site.register(FAQ,FAQAdmin)
+
+
+
---
+++ product/models.py
@@ -0,0 +1,138 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from django.db.models import Avg, Count
+from django.forms import ModelForm
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+
+
+
+class Category(MPTTModel):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    parent = TreeForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    keywords = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    status=models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField(null=False, unique=True)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+    def __str__(self):                           # __str__ method elaborated later in
+        full_path = [self.title]                  # post.  use __unicode__ in place of
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' / '.join(full_path[::-1])
+
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #many to one relation with Category
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    image=models.ImageField(upload_to='images/',null=False)
+    price = models.DecimalField(max_digits=12, decimal_places=2,default=0)
+    amount=models.IntegerField(default=0)
+    slug = models.SlugField(null=False, unique=True)
+    status=models.CharField(max_length=10,choices=STATUS)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+
+    ## method to create a fake table field in read only mode
+    def image_tag(self):
+        if self.image.url is not None:
+            return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+        else:
+            return ""
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+    def avaregereview(self):
+        reviews = Comment.objects.filter(product=self, status='True').aggregate(avarage=Avg('rate'))
+        avg=0
+        if reviews["avarage"] is not None:
+            avg=float(reviews["avarage"])
+        return avg
+
+    def countreview(self):
+        reviews = Comment.objects.filter(product=self, status='True').aggregate(count=Count('id'))
+        cnt=0
+        if reviews["count"] is not None:
+            cnt = int(reviews["count"])
+        return cnt
+
+
+class Comment(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50, blank=True)
+    comment = models.CharField(max_length=250,blank=True)
+    rate = models.IntegerField(default=1)
+    ip = models.CharField(max_length=20, blank=True)
+    status=models.CharField(max_length=10,choices=STATUS, default='New')
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['subject', 'comment', 'rate']
+
+
+class ProductLang(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) #many to one relation with Category
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    slug = models.SlugField(null=False, unique=True)
+    detail=RichTextUploadingField()
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'slug': self.slug})
+
+class CategoryLang(models.Model):
+    category = models.ForeignKey(Category, related_name='categorylangs', on_delete=models.CASCADE) #many to one relation with Category
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    slug = models.SlugField(null=False, unique=True)
+    description = models.CharField(max_length=255)
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
